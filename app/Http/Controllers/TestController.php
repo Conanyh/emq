6733cd557e9a470d4db3cc8c155ab3c2c5f0e27d
Conanyh <ycoralh@163.com>
2020-03-18 17:39:25 +0800

    test queue

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,7 @@ class TestController extends Controller
 {
     public function store(Request $request)
     {
+        echo 'this is queue';
         $data['title'] = $request->get('title', 'this is test queue');
         $data['time'] = date('Y-m-d H:i:s');
         $job = (new TestQueue($data))->onQueue('testqueue');
